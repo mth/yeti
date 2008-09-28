6109a83de7044fed0afb0126b7036d7ca9b11cdc
Madis Janson <madis@cyber.ee>
2008-09-29 01:10:44 +0300

    nullpointerexceptions...

--- c/YetiBuiltins.java
+++ c/YetiBuiltins.java
@@ -703,14 +703,19 @@ interface YetiBuiltins extends CaseCode {
                 }
 
                 void binGen(Ctx ctx, Code arg1, Code arg2) {
+                    String lclass = "yeti/lang/LList";
+                    if (arg2.type.deref().param[1].deref()
+                            != YetiType.NO_TYPE) {
+                        lclass = "yeti/lang/LMList";
+                    }
                     ctx.visitLine(line);
-                    ctx.visitTypeInsn(NEW, "yeti/lang/LList");
+                    ctx.visitTypeInsn(NEW, lclass);
                     ctx.visitInsn(DUP);
                     arg1.gen(ctx);
                     arg2.gen(ctx);
                     ctx.visitLine(line);
                     ctx.visitTypeInsn(CHECKCAST, "yeti/lang/AList");
-                    ctx.visitInit("yeti/lang/LList",
+                    ctx.visitInit(lclass,
                                   "(Ljava/lang/Object;Lyeti/lang/AList;)V");
                     ctx.forceType("yeti/lang/AList");
                 }
