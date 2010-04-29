7abe61c54fc4e1e8af77fea5a460ef4084210fe6
Madis Janson <madis@cyber.ee>
2010-04-29 09:14:03 +0300

    ...

--- lib/Struct.java
+++ lib/Struct.java
@@ -3,7 +3,7 @@
 /**
  * Yeti core library - structure interface.
  *
- * Copyright (c) 2007-2010 Madis Janson
+ * Copyright (c) 2010 Madis Janson
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
