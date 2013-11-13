300ab7d2c851c7e4269bd50c1d046ea18937e428
Madis Janson <madis@cyber.ee>
2013-11-13 16:15:43 +0200

    remove unused HashMap import

--- c/YetiType.java
+++ c/YetiType.java
@@ -36,7 +36,6 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
-import java.util.HashMap;
 import java.util.IdentityHashMap;
 import java.util.Iterator;
 
